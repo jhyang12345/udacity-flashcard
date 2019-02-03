2122cdd86575ad76921feb7e81e2e0af4eec15ca
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-02-04 02:03:22 +0900

    Got stack navigator to show

--- App.js
+++ App.js
@@ -10,17 +10,26 @@ import { createAppContainer,
         createDrawerNavigator } from 'react-navigation'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { Constatns, Constants } from 'expo'
 import reducer from './reducers'
 import DeckView from './components/DeckView'
 import AddDeckView from './components/AddDeckView'
+import {purple, white} from "./utils/colors"
 
 
 export default class App extends React.Component {
+
+  componentDidMount = () => {
+    console.log(this.props)
+    
+  }
+
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          
+          <AppStatusBar backgroundColor={"#333"} barStyle='light-content'/>
+          <Text>Something to show</Text>
           <AppContainer />
         </View>
       </Provider>
@@ -48,21 +57,51 @@ const RouteConfigs = {
 const TabNavigatorConfig = {
   navigationOptions: {
     header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === "ios" ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === "ios" ? white : purple,
+      shadowColor: "rgba(0, 0, 0, 0.24)",
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
   }
-}
+};
 
-const Tabs =
-  Platform.OS === "ios"
-  ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
-  : createDrawerNavigator(RouteConfigs, TabNavigatorConfig)
+const Tabs = createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+  // Platform.OS === "ios"
+  //   ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+  //   : createBottomTabNavigator(RouteConfigs, TabNavigatorConfig);
 
 const AppContainer = createAppContainer(Tabs)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Something</Text>
+      </View>
+    )
+  }
+}
+
+function AppStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}></StatusBar>
+    </View>
+  )
+}
